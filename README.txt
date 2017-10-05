d975274369242f7acb791c92145b238d0466cfcb
Per Dalsten Sorensen <pdalsten@gmail.com>
2017-10-04 20:31:41 -0400

    Initial check-in

---
+++ README.txt
@@ -0,0 +1,3 @@
+Create new directory, e.g. ~/Projects/MusicLibraryConfig and do 'git init'.
+
+Add properties/yml for specific applications (and application.yml for general properties) and git commit. 
---
+++ src/main/java/dk/purplegreen/musiclibrary/config/MusicLibraryConfigService.java
@@ -0,0 +1,13 @@
+package dk.purplegreen.musiclibrary.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class MusicLibraryConfigService {
+	public static void main(String[] args) {
+		SpringApplication.run(MusicLibraryConfigService.class, args);
+	}
+}
