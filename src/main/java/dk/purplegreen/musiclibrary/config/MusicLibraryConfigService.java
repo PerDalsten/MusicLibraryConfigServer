a1dadb1952504365495e98809ef7c0345ca3e4dd
Per Dalsten Sorensen <pdalsten@gmail.com>
2017-11-13 18:14:14 -0500

    CORS support

--- src/main/java/dk/purplegreen/musiclibrary/config/MusicLibraryConfigService.java
+++ src/main/java/dk/purplegreen/musiclibrary/config/MusicLibraryConfigService.java
@@ -3,11 +3,25 @@ package dk.purplegreen.musiclibrary.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableConfigServer
 @SpringBootApplication
 public class MusicLibraryConfigService {
 	public static void main(String[] args) {
 		SpringApplication.run(MusicLibraryConfigService.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
 	}
 }
