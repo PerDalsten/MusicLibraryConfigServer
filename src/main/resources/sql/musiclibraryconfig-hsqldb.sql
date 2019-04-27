b8d5d49646faaa03cfe594a70a0465f5ddd0760f
Per Dalsten Sorensen <pdalsten@gmail.com>
2019-04-27 15:16:46 -0400

    Updated column names

--- src/main/resources/sql/musiclibraryconfig-hsqldb.sql
+++ src/main/resources/sql/musiclibraryconfig-hsqldb.sql
@@ -4,9 +4,9 @@ CREATE TABLE configuration
 	application VARCHAR(100) NOT NULL,
     profile VARCHAR(100) DEFAULT 'default' NOT NULL,
     label VARCHAR(100) DEFAULT 'master' NOT NULL,
-    key VARCHAR(200) NOT NULL,
-    value VARCHAR(200) NOT NULL,
-    CONSTRAINT configuration_uniq UNIQUE (application, profile, label, key)
+    config_key VARCHAR(200) NOT NULL,
+    config_value VARCHAR(200) NOT NULL,
+    CONSTRAINT configuration_uniq UNIQUE (application, profile, label, config_key)
 );
 
 CREATE USER "musiclibraryconfig" password 'musiclibraryconfig';
@@ -16,4 +16,3 @@ CREATE ROLE "musiclibraryconfig_role";
 GRANT "musiclibraryconfig_role" to "musiclibraryconfig";
 
 GRANT ALL ON TABLE configuration to "musiclibraryconfig_role";
-
